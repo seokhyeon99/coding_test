4dd322f454c5b54970ea5f59f0d3e08d211c686e
seokhyeon99 <lsh9058@naver.com>
2020-10-03 10:51:08 +0900

    1003

--- 07. 문자열/백준07.py
+++ 07. 문자열/백준07.py
@@ -17,7 +17,18 @@ import sys
 a = sys.stdin.readline().strip()
 for i in range(97,123):
     print(a.find(chr(i)), end=" ")
-    
+
+#2675
+import sys
+a = sys.stdin.readline().strip()
+for i in range(int(a)):
+    b,c = sys.stdin.readline().strip().split()
+    for j in range(len(c)):
+        for k in range(int(b)):
+            print(c[j:j+1], end='')
+    print()
+
+
 #1157
 import sys
 a = sys.stdin.readline().strip()
@@ -35,4 +46,51 @@ if first == second:
     print('?')
 else:
     print(chr(first_index+65))
-
+    
+#1152
+import sys
+a = sys.stdin.readline().strip()
+count = 0
+for i in range(len(a)):
+    if a[i:i+1] == ' ':
+        count += 1
+if a == ' '*len(a):
+  print('0')
+else:
+  print(count+1)
+  
+#2908
+import sys
+a,b = sys.stdin.readline().strip().split()
+c=0
+d=0
+for i in range(3):
+    c += int(a[i:i+1]) * (10**(i))
+    d += int(b[i:i+1]) * (10**(i))
+if c>d:
+    print(c)
+else:
+    print(d)
+    
+#5622
+import sys
+a = sys.stdin.readline().strip()
+time = 0
+for i in range(len(a)):
+    if 65<=ord(a[i:i+1])<=67:
+         time += 3
+    elif (68<=ord(a[i:i+1])<=70):
+         time += 4
+    elif (71<=ord(a[i:i+1])<=73):
+         time += 5
+    elif (74<=ord(a[i:i+1])<=76):
+         time += 6
+    elif (77<=ord(a[i:i+1])<=79):
+         time += 7
+    elif (80<=ord(a[i:i+1])<=83):
+         time += 8
+    elif (84<=ord(a[i:i+1])<=86):
+         time += 9
+    elif (87<=ord(a[i:i+1])<=90):
+         time += 10
+print(time)
