f5e9d0a7119ea88c7c8174b536aa7165c01b34bb
Seokhyeon Lee <68173433+seokhyeon99@users.noreply.github.com>
2025-09-05 21:08:28 +0900

    [Silver IV] Title: 듣보잡, Time: 2504 ms, Memory: 44192 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,15 +1,16 @@
-n, m = map(int, input().split())
-notListened = set()
-notSeen = set()
-for i in range(n):
-    notListened.add(input())
-for i in range(m):
-    notSeen.add(input())
-notListenedAndSeen = []
-for i in notSeen:
-    if i in notListened:
-        notListenedAndSeen.append(i)
-notListenedAndSeen.sort()
-print(len(notListenedAndSeen))
-for i in notListenedAndSeen:
+a, b = map(int, input().split())
+notheard = set()
+notseen = set()
+for i in range(a):
+    notheard.add(input())
+for i in range(b):
+    notseen.add(input())
+
+notheadandnotseen=[]
+for i in notseen:
+    if i in notheard:
+        notheadandnotseen.append(i)
+print(len(notheadandnotseen))
+notheadandnotseen.sort()
+for i in notheadandnotseen:
     print(i)
