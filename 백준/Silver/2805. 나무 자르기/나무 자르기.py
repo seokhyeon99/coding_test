3bf1ffa3b5cfec467ad362ac26e4eb482b9b5b5c
Seokhyeon Lee <68173433+seokhyeon99@users.noreply.github.com>
2025-09-23 22:29:06 +0900

    [Silver II] Title: 나무 자르기, Time: 1224 ms, Memory: 148224 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int,input().split())
+tree = sorted(list(map(int, input().split())))
+copied_tree = tree.copy()
+
+for i in range(n):
+    tree[i] -= tree[n-1]
+
+# print(tree)
+height = 0
+
+for i in range(0,n):
+    # print(m)
+    if i == n-1:
+        if m%(i+1):
+            divided_m = m//(i+1)+1
+        else:
+            divided_m = m//(i+1)
+        height = copied_tree[0] - divided_m
+        break
+    elif (i+1)*(tree[n-i-1] - tree[n-i-2]) >= m:
+        # print(copied_tree[n-1], copied_tree[n-i-1], m//(i+1))
+        if m%(i+1):
+            divided_m = m//(i+1)+1
+        else:
+            divided_m = m//(i+1)
+        height = copied_tree[n-i-1] - divided_m
+        break
+    else:
+        m -= (i+1) * (tree[n-i-1] - tree[n-i-2])
+
+print(height)
