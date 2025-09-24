7b27cc57d8eb632735adce8f99d8d369af2487d2
Seokhyeon Lee <68173433+seokhyeon99@users.noreply.github.com>
2025-09-24 13:32:24 +0900

    Update 나무 자르기.py

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,6 +1,35 @@
 import sys
 input = sys.stdin.readline
 
+n, m = map(int, input().split())
+tree = sorted(list(map(int, input().split())))
+
+# range of height: 0 ~ max(tree)
+start = 0
+end = max(tree)
+height = 0
+result = 0
+while(True):
+    if start > end:
+        break    
+    height = (start+end)//2
+    sum = 0
+    for i in range(n):
+        if tree[i]-height > 0:
+            sum += tree[i]-height
+    if sum >= m:
+        start = height+1
+        result = height
+    else: # sum < m
+        end = height-1
+print(result)
+    
+
+
+'''
+import sys
+input = sys.stdin.readline
+
 n, m = map(int,input().split())
 tree = sorted(list(map(int, input().split())))
 copied_tree = tree.copy()
@@ -8,11 +37,9 @@ copied_tree = tree.copy()
 for i in range(n):
     tree[i] -= tree[n-1]
 
-# print(tree)
 height = 0
 
 for i in range(0,n):
-    # print(m)
     if i == n-1:
         if m%(i+1):
             divided_m = m//(i+1)+1
@@ -21,7 +48,6 @@ for i in range(0,n):
         height = copied_tree[0] - divided_m
         break
     elif (i+1)*(tree[n-i-1] - tree[n-i-2]) >= m:
-        # print(copied_tree[n-1], copied_tree[n-i-1], m//(i+1))
         if m%(i+1):
             divided_m = m//(i+1)+1
         else:
@@ -32,3 +58,4 @@ for i in range(0,n):
         m -= (i+1) * (tree[n-i-1] - tree[n-i-2])
 
 print(height)
+'''
