58c347ba1b7e11adaa92feac4bf2e9d7f418d19a
Seokhyeon Lee <68173433+seokhyeon99@users.noreply.github.com>
2025-10-01 14:37:10 +0900

    [Silver I] Title: 숨바꼭질, Time: 108 ms, Memory: 36944 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,40 +1,20 @@
 import sys
 input = sys.stdin.readline
+from collections import deque
 
 n, k = map(int, input().split())
-min_time = [-1]*100001
-min_time[n] = 0
-queue = [n]
-# print(queue)
-i = 0
-while queue:
-    if min_time[k] != -1:
-        break
-    if queue[i]+1 >= 0 and queue[i]+1 <= 100000 and min_time[queue[i]+1] == -1:
-        min_time[queue[i]+1] = min_time[queue[i]]+1
-        queue.append(queue[i]+1)
-        # print(queue[i]+1, min_time[queue[i]+1])
-    if queue[i]-1 >= 0 and queue[i]-1 <= 100000 and min_time[queue[i]-1] == -1:
-        min_time[queue[i]-1] = min_time[queue[i]]+1
-        queue.append(queue[i]-1)
-        # print(queue[i]-1, min_time[queue[i]-1])
-    if queue[i]*2 >= 0 and queue[i]*2 <= 100000 and min_time[queue[i]*2] == -1:
-        min_time[queue[i]*2] = min_time[queue[i]]+1
-        queue.append(queue[i]*2)
-        # print(queue[i]*2, min_time[queue[i]*2])
-    i += 1
-# for i in range(20):
-#     print(i, end= ' ')
-# print()
-# for i in range(20):
-#     print(min_time[i], end=' ')
-# print()
-print(min_time[k])
-
-# for i in range(n-1, -1, -1):
-#     min_time[i] = min(min_time[2*i], min_time[i+1]+1, min_time[i-1])+1
-# for i in range(n+1, k+1):
-#     if i%2==0:
-#         min_time[i] = min(min_time[n//2], min_time[n-1], min_time[n+1]) + 1
-#     else:
-#         min_time[i] = min(min_time[n-1], min_time[n+1]) + 1
+if k < n:
+    print(n-k)
+else:
+    min_time = [-1]*100001
+    min_time[n] = 0
+    queue = deque([n])
+    while queue:
+        cur_pos = queue.popleft()
+        if min_time[k] != -1:
+            break
+        for next_pos in [cur_pos+1, cur_pos-1, cur_pos*2]:
+            if next_pos >= 0 and next_pos <= 100000 and min_time[next_pos] == -1:
+                min_time[next_pos] = min_time[cur_pos]+1
+                queue.append(next_pos)
+    print(min_time[k])
