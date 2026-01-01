bd3e4d6b0cbf95b135fa5d38735c4758ed60a521
Seokhyeon Lee <68173433+seokhyeon99@users.noreply.github.com>
2026-01-01 21:53:29 +0900

    [Bronze II] Title: 블랙잭, Time: 76 ms, Memory: 38784 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+card = list(map(int, input().split()))
+
+sum = 0
+result = []
+
+for i in range(n-2):
+    for j in range(i+1,n-1):
+        for k in range(j+1, n):
+            sum = card[i] + card[j] + card[k]
+            if sum <= m:
+                result.append(sum)
+
+print(max(result))
