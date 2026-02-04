84cee71dd186268341f89a8352fe32f3e882af10
Seokhyeon Lee <68173433+seokhyeon99@users.noreply.github.com>
2026-02-04 21:55:44 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,4 +1,3 @@
-'''
 n_com = int(input())
 n_rel = int(input())
 rel = [[0]*2]*n_rel
@@ -22,34 +21,3 @@ while True:
             # print(rel[0])
     final_err_lst.append(err)
 print(len(final_err_lst)-1)
-'''
-
-
-
-from collections import deque
-n_node = int(input())
-n_edge = int(input())
-
-linked = [[0]*(n_node+1) for _ in range(n_node+1)]
-for i in range(n_edge):
-    x, y = map(int, input().split())
-    linked[x][y] = 1
-    linked[y][x] = 1
-
-# print(linked)
-hacked = [0]*(n_node+1)
-hacked[1] = 1
-queue = deque([1])
-count = 0
-
-while queue:
-    # print(queue)
-    current_node = queue.popleft()
-    # print(current_node)
-    for i in range(n_node+1):
-        if linked[current_node][i] and not hacked[i]:
-            queue.append(i)
-            hacked[i] = 1
-            count += 1
-            # print(count)
-print(count)
