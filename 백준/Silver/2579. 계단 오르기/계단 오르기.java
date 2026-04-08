c5147f1e7ef7371c4616cdb2b0c8afa1d7fab7f3
Seokhyeon Lee <68173433+seokhyeon99@users.noreply.github.com>
2026-04-08 17:52:16 +0900

    [Silver III] Title: 계단 오르기, Time: 64 ms, Memory: 11604 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+/*
+ * f(n) = max(s(n-1) + f(n-3), f(n-2)) + s(n)
+ */
+
+public class Main {
+	
+	static int[] dp, stair;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		stair = new int[N+1];
+		for (int i = 1; i < N+1; i++) {
+			stair[i] = Integer.parseInt(br.readLine()); 
+		}
+		dp = new int[N+1];
+		dp[1] = stair[1];
+		if(N>=2) dp[2] = stair[1] + stair[2];
+		if(N>=3) dp[3] = Math.max(stair[2], dp[1]) + stair[3];
+		if(N>=4) {
+			for (int i = 4; i <= N; i++) {
+				dp[i]= Math.max(dp[i-3] + stair[i-1], dp[i-2]) + stair[i]; 
+				
+			}
+		}
+		System.out.println(dp[N]);
+	}
+}
